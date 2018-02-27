99d55a9e7d63d0c8ae232aed613c3931372c1e06
Brian Moore <bmoore6@ucmerced.edu>
2018-02-26 16:37:56 -0800

    Board now rendering after player select

--- Projects/tictactoe/app_main.cpp
+++ Projects/tictactoe/app_main.cpp
@@ -1,4 +1,5 @@
 #include <GL/freeglut.h>
+#include <iostream>
 #include <vector>
 #include "Square.h"
 
@@ -11,24 +12,10 @@ int width = 800, height = 800, players, gameState;
 Square oneP(-0.5, 0.0, 0.5), twoP(0.5, 0.0, 0.5);
 std::vector<Square> board;
 
-void onClick(int button, int state, int x, int y){
-
-    float tx = x;
-    float ty = y;
-    float mx =  (tx / 400 - 1.0);
-    float my = -(ty / 400 - 1.0);
-
-}
-
-void onPress(unsigned char key, int x, int y){
-
-    if(key == 27){
-        exit(0);
-    }
-
-}
 
 void drawSquare(Square s){
+
+    std::cout << "(" << s.getX() <<  ", " << s.getY() << ")" << std::endl;
 
     glRectf(s.getX() - (0.5 * s.getL()), s.getY() - (0.5 * s.getL()), 
             s.getX() + (0.5 * s.getL()), s.getY() + (0.5 * s.getL()));
@@ -37,6 +24,15 @@ void drawSquare(Square s){
 
 void drawBoard(){
 
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glColor3f(0.0, 1.0, 0.0);
+    for(int i = 0; i < board.size(); i++){
+        drawSquare(board[i]);
+    }
+
+    glutSwapBuffers();
 
 }
 
@@ -66,7 +62,64 @@ void drawPlayerSelect(){
 
 }
 
+void onClick(int button, int state, int x, int y){
+
+    float tx = x;
+    float ty = y;
+    float mx =  (tx / 400 - 1.0);
+    float my = -(ty / 400 - 1.0);
+
+    if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
+
+        switch(gameState){
+
+            case PLAYER_SELECT:
+
+                if(oneP.contains(mx, my)){
+                    players = 1;
+                    gameState = PLAYING;
+                    drawBoard();
+                }else if(twoP.contains(mx, my)){
+                    players = 2;
+                    gameState = PLAYING;
+                    drawBoard();
+                }
+
+                break;
+        }
+
+    }
+
+
+}
+
+void onPress(unsigned char key, int x, int y){
+
+    if(key == 27){
+        exit(0);
+    }
+
+}
+
 int main(int argc, char** argv){
+
+    // Populate board with squares
+    float lastX = -1.32, lastY = 0.66;
+    for(int i = 0; i < 9; i++){
+
+        if(i == 3 || i == 6){
+            Square s(-0.66, lastY - 0.66, 0.64);
+            board.push_back(s);
+            lastX = s.getX();
+            lastY = s.getY();
+        }else{
+            Square s(lastX + 0.66, lastY, 0.64);
+            board.push_back(s);
+            lastX = s.getX();
+            lastY = s.getY();
+        }
+ 
+    }
 
     // Initialize GLUT
     glutInit(&argc, argv);
